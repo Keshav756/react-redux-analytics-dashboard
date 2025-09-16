83b471293d44ef39c46aaac8cdd8b7a37f4109ff
Keshav756 <144075944+Keshav756@users.noreply.github.com>
2025-09-16 10:31:38 +0530

    Delete backend directory

--- backend/dist/controllers/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-export declare class UserController {
-    static register(req: Request, res: Response, next: NextFunction): Promise<void>;
-    static login(req: Request, res: Response, next: NextFunction): Promise<void>;
-    static getProfile(req: Request, res: Response, next: NextFunction): Promise<void>;
-    static updateProfile(req: Request, res: Response, next: NextFunction): Promise<void>;
-}
-export default UserController;
-//# sourceMappingURL=user.controller.d.ts.map
--- backend/dist/routes/path.routes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export default router;
-//# sourceMappingURL=path.routes.d.ts.map
